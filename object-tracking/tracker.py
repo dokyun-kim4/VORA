4b98c3caabf93d9b7548a013bcfed3af80114e3d
dfriishecht <dfriishecht@olin.edu>
2023-11-14 14:01:55 -0500

    Add import statements

---
+++ object-tracking/tracker.py
@@ -0,0 +1,8 @@
+#TODO: setup YOLO to only track and classify objects in our list
+import cv2
+from ultralytics import YOLO
+from ultralytics.utils.plotting import Annotator
+import torch
+import numpy as np
+
+
