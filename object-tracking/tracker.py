c417bcfe2dc38b4f9fad1bf8015dd8d4d39707a7
dfriishecht <dfriishecht@olin.edu>
2023-11-16 22:03:13 -0500

    print conf values

--- object-tracking/tracker.py
+++ object-tracking/tracker.py
@@ -10,20 +10,31 @@ model = YOLO("yolov8m.pt")
 cap = cv2.VideoCapture(0)
 classified_objects = [39, 41, 64, 67, 73, 76]
 
+def extract_confidence_bbox(results):
+    for result in results:
+        bounding_box = result.boxes
+        box_xyxy = bounding_box.xyxy.cpu().numpy()
+        box_conf = bounding_box.conf.cpu().numpy()
+    return box_xyxy, box_conf
+
 while cap.isOpened():
     success, frame = cap.read()
     results = model.predict(frame, classes=classified_objects, verbose=False)
-
+    list, conf = extract_confidence_bbox(results)
+    print(list)
+    print(conf)
     for r in results:
         annotator = Annotator(frame)
-
         bboxes = r.boxes
+        bound_box = bboxes.xyxy
+        prob = bboxes.conf.cpu().numpy()
         box_count = 0
         for box in bboxes:
             box_count += 1
             box_coord = box.xyxy[0]
             boxclass = box.cls
             annotator.box_label(box_coord, f"{model.names[int(boxclass)]} {box_count}")
+
     
     annotated_frame = annotator.result()
     cv2.imshow("YOLO", annotated_frame)
