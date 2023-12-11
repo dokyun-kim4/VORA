515489ba44a826155066496b35c64e2ab127381b
dokyun-kim4 <donny5557@gmail.com>
2023-12-11 13:14:44 -0500

    rename `tracker.py` to `tracker_demo.py`, add demo instructions

--- object_tracking/tracker_demo.py
+++ object_tracking/tracker_demo.py
@@ -1,11 +1,8 @@
-#TODO: setup YOLO to only track and classify objects in our list
+# You can run this file on its own to see how our object tracker works
 import cv2
 from ultralytics import YOLO
 from ultralytics.utils.plotting import Annotator
-import torch
-import numpy as np
 import helpers as h
-from sort import Sort
 import time
 
 model = YOLO("yolov8m.pt")
@@ -13,6 +10,9 @@ cap = cv2.VideoCapture(0)
 
 classified_objects = [39, 41, 64, 67, 73, 76]
 classified_objects_names = h.object_sortkey
+
+# change this to other objects in classified_object_names
+objToTrack = "cup"
 
 for _ in range(10):
 # Take a few frames so the camera can adjust
@@ -31,24 +31,18 @@ while cap.isOpened():
         conf_box = h.get_obj_from_df(result_df,name)
         conf_box_all[name] = conf_box
 
-
-
-
     track_all = {}
     for i,name in enumerate(classified_objects_names):
         crnt_conf_box = conf_box_all[name]
-        print(name)
-        print(crnt_conf_box)
+        # print(name)
+        # print(crnt_conf_box)
         if len(crnt_conf_box) != 0:
- 
             track_all[name] = h.sorter[i].update(crnt_conf_box)
-
         else:
-
             track_all[name] = h.sorter[i].update()
 
     xy = h.find_obj("cup",track_all)
-    print(xy)
+    # print(xy)
     if xy:
         cv2.circle(frame,xy,3,(0,0,255),-1)
 
@@ -59,7 +53,7 @@ while cap.isOpened():
             cv2.rectangle(frame,(x1,y1),(x2,y2),(0,0,255),2)
             cv2.putText(frame,f"{key} {str(id)}",(x1+10,y1+40),cv2.FONT_HERSHEY_PLAIN,2,(0,0,255),2)
 
-    cv2.imshow("YOLO",frame)
+    cv2.imshow("Object Tracker",frame)
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 cap.release()
